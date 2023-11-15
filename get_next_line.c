dd337cb4aab3b39de036a0bd41c2d8c9c73d6e5a
Alex Pyykonen <alexpyykonen@gmail.com>
2023-11-15 11:49:05 +0200

    yolo

--- get_next_line.c
+++ get_next_line.c
@@ -13,9 +13,9 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-static char	*ft_cat_free(char *s_buffer, char *read_buffer)
+static char *ft_cat_free(char *s_buffer, char *read_buffer)
 {
-	char	*joined;
+	char *joined;
 
 	joined = NULL;
 	joined = ft_strjoinni(s_buffer, read_buffer);
@@ -25,22 +25,22 @@ static char	*ft_cat_free(char *s_buffer, char *read_buffer)
 }
 
 // Finds eof or newline
-static char	*ft_next_spot(char *buffer)
+static char *ft_next_spot(char *buffer)
 {
-	size_t		i;
-	size_t		j;
-	size_t		line_size;
-	char		*newline;
+	size_t i;
+	size_t j;
+	size_t line_size;
+	char *newline;
 
 	i = 0;
 	j = 0;
 	while (buffer[i] && buffer[i] != '\n')
 		i++;
-    if (!buffer[i])
+	if (!buffer[i])
 	{
 		if (buffer)
 			free(buffer);
-        return (NULL);
+		return (NULL);
 	}
 	line_size = ft_strlenni(buffer) - i + 1;
 	newline = malloc(line_size * sizeof(char) + 1);
@@ -55,10 +55,10 @@ static char	*ft_next_spot(char *buffer)
 	return (newline);
 }
 
-static char	*ft_getline(char *s_buffer)
+static char *ft_getline(char *s_buffer)
 {
-	char	*line;
-	size_t	i;
+	char *line;
+	size_t i;
 
 	i = 0;
 	if (!s_buffer[i])
@@ -75,19 +75,21 @@ static char	*ft_getline(char *s_buffer)
 		line[i] = s_buffer[i];
 		i++;
 	}
-	if (s_buffer[i] && s_buffer[i] == '\n'){
+	if (s_buffer[i] && s_buffer[i] == '\n')
+	{
 		line[i++] = '\n';
-	}else{
+	}
+	else
+	{
 		line[i] = '\0';
 	}
-	//printf("here --> %s\n", line);
 	return (line);
 }
 
-static char	*ft_read_file(int fd, char *s_buffer)
+static char *ft_read_file(int fd, char *s_buffer)
 {
-	char		*read_buffer;
-	ssize_t		bytes_read;
+	char *read_buffer;
+	ssize_t bytes_read;
 
 	read_buffer = NULL;
 	bytes_read = 1;
@@ -121,20 +123,18 @@ static char	*ft_read_file(int fd, char *s_buffer)
 			if (!s_buffer)
 				return (NULL);
 			if (ft_check_nl(read_buffer))
-				break ;
+				break;
 		}
 	}
 	free(read_buffer);
 	return (s_buffer);
 }
 
-char	*get_next_line(int fd)
+char *get_next_line(int fd)
 {
-	static char		*buffer;
-	char			*line;
-	//static int i = 0;
+	static char *buffer;
+	char *line;
 
-	//printf("filed --> %d\n", fd);
 	line = NULL;
 	if ((fd < 0 || BUFFER_SIZE <= 0) || read(fd, 0, 0) < 0)
 	{
@@ -146,7 +146,6 @@ char	*get_next_line(int fd)
 	if (!buffer)
 		return (NULL);
 	line = ft_getline(buffer);
-	//printf("line%d --> %s\n", i++, line);
 	if (!line)
 	{
 		if (buffer)
@@ -160,93 +159,87 @@ char	*get_next_line(int fd)
 	return (line);
 }
 
-/*
 int main(int ac, char **av)
 {
-    (void)ac;
-    int fd = open(av[1], O_RDONLY);
-    char *line;
+	(void)ac;
+	int fd = open(av[1], O_RDONLY);
+	char *line;
+	/*
+		line = NULL;
+			line = get_next_line(fd);
+			printf("line 1 a  --> %s\n", line);
+			 line = get_next_line(fd);   //"bbbbbbbbbb\n""aaaaaaaaaa\n"
+			printf("line 2 b --> %s\n", line);
+			// set the next read call to return -1
+			if (BUFFER_SIZE > 100) {
+				char *temp;
+				do {
+					temp = get_next_line(fd);
+					printf("temp --> %s\n", temp);
+					free(temp);
+				} while (temp != NULL);
+			}
+			/ line = get_next_line(fd); // NULL
+			printf("line 3 null b --> %s\n", line);
+			close(fd);
+			free(line);*/
+	// fd = open(av[1], O_RDONLY);
+	line = get_next_line(fd); //"aaaaaaaaaa\n""bbbbbbbbbb\n""cccccccccc\n" "dddddddddd\n"
+	printf("line  4 a --> %s", line);
+	line = get_next_line(fd);
+	printf("line 5 b  --> %s\n", line);
+	// line = get_next_line(fd);
+	// printf("line --> %s\n", line);
+	// line = get_next_line(fd);
+	// printf("line --> %s\n", line);
+	// line = get_next_line(fd); // NULL
+	// printf("line --> %s\n", line);
 
-    line = NULL;
-		line = get_next_line(fd);
-		printf("line 1 a  --> %s\n", line);
-		 line = get_next_line(fd);   //"bbbbbbbbbb\n""aaaaaaaaaa\n"
-		printf("line 2 b --> %s\n", line);
-		// set the next read call to return -1
-		if (BUFFER_SIZE > 100) {
-			char *temp;
-			do {
-				temp = get_next_line(fd);
-				printf("temp --> %s\n", temp);
-				free(temp);
-			} while (temp != NULL);
-		}
-		/ line = get_next_line(fd); // NULL
-		printf("line 3 null b --> %s\n", line);
-		close(fd);
-		free(line);
-		fd = open(av[1], O_RDONLY);
-		line = get_next_line(fd);  //"aaaaaaaaaa\n""bbbbbbbbbb\n""cccccccccc\n" "dddddddddd\n"
-		printf("line  4 a --> %s\n", line);
-		 line = get_next_line(fd);
-		printf("line 5 b  --> %s\n", line);
-		 line = get_next_line(fd);
-		printf("line --> %s\n", line);
-		 line = get_next_line(fd);
-		printf("line --> %s\n", line);
-		 line = get_next_line(fd); // NULL
-		printf("line --> %s\n", line);
+	//  line = get_next_line(fd);
+	//  write(1, line, ft_strlenni(line));
 
+	// line = get_next_line(fd);
+	//  write(1, line, ft_strlenni(line));
+	/*line = get_next_line(fd);
+	printf("%s", line);
+	line = get_next_line(fd);
+	printf("%s", line);
+	line = get_next_line(fd);
+	printf("%s", line);
+	line = get_next_line(fd);
+	printf("%s", line);
+	line = get_next_line(fd);
+	printf("%s", line);
+*/
+	// line = get_next_line(fd);
 
-   //  line = get_next_line(fd);
-  //  write(1, line, ft_strlenni(line));
-
-
-
-
-   //line = get_next_line(fd);
+	//  if (!line)
+	//	printf("cor");
+	// line = get_next_line(fd);
+	// if (!line)
+	// printf("correct\n");
+	// printf("char n --> %d", *(line + ft_strlenni(line) - 1));
+	//   printf("char last --> %c", *(line + ft_strlenni(line) - 2));
 	// write(1, line, ft_strlenni(line));
-	   line = get_next_line(fd);
-	   printf("%s", line );
-	     line = get_next_line(fd);
-	   printf("%s", line );
-	      line = get_next_line(fd);
-	   printf("%s", line );
-	      line = get_next_line(fd);
-	  printf("%s", line );
-	 line = get_next_line(fd);
-	   printf("%s", line );
-	  
-	  
-	  // line = get_next_line(fd);
+	//   line = get_next_line(fd);
+	// write(1, line, ft_strlenni(line));
+	//   line = get_next_line(fd);
+	// write(1, line, ft_strlenni(line));
 
-	 //  if (!line)
-	   //	printf("cor");
-	   // line = get_next_line(fd);
-		//if (!line)
-			//printf("correct\n");
-	  // printf("char n --> %d", *(line + ft_strlenni(line) - 1));
-	 //   printf("char last --> %c", *(line + ft_strlenni(line) - 2));
+	// line = get_next_line(fd);
 	// write(1, line, ft_strlenni(line));
-	 //   line = get_next_line(fd);
+
+	// line = get_next_line(fd);
 	// write(1, line, ft_strlenni(line));
-	 //   line = get_next_line(fd);
+	// line = get_next_line(fd);
 	// write(1, line, ft_strlenni(line));
-    
-   // line = get_next_line(fd);
-   // write(1, line, ft_strlenni(line));
-    
-    //line = get_next_line(fd);
-   // write(1, line, ft_strlenni(line));
-    //line = get_next_line(fd);
-   // write(1, line, ft_strlenni(line));
-    //free(line);
-    close(fd);
-    //write(1, line, ft_strlenni(line));
-    return 0;
+	// free(line);
+	close(fd);
+	// write(1, line, ft_strlenni(line));
+	return 0;
 }
 
-
+/*
 	title("files/41_no_nl: ")
 	fd = open("files/41_no_nl", O_RDWR);
 	TEST(gnl(fd, "01234567890123456789012345678901234567890");
